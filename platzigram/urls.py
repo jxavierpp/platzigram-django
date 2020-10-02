c52b4602174117fb4e0e436fd5d597367d5a2c7d
Xavier Paredes <jxavierpp1@gmail.com>
2020-10-01 23:26:32 -0700

    🥚 hello world url added to dispatcher

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from platzigram import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello-world/', views.hello_word),
 ]
