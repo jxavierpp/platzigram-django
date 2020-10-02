2b7ecde67ce24d835f9c5f7fa42bead8fce4119f
Xavier Paredes <jxavierpp1@gmail.com>
2020-10-02 00:26:59 -0700

    ♻ new route and view added

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("hello-world/", views.hello_word),
     path("sorted/", views.sorted_numbers),
+    path("hi/<str:name>/<int:age>/", views.say_hi),
 ]
--- platzigram/views.py
+++ platzigram/views.py
@@ -14,7 +14,7 @@ def hello_word(request):
     return HttpResponse("Oh, hi! Current server time is {now}".format(now=now))
 
 def sorted_numbers(request):
-    """Hi"""
+    """Sort numbers."""
     numbers = [int(number) for number in request.GET['numbers'].split(",")]
     sorted_numbers = sorted(numbers)
     data = {
@@ -23,3 +23,11 @@ def sorted_numbers(request):
         'message': 'Integers sorted successfuly.'
     }
     return HttpResponse(json.dumps(data), content_type='application/json')
+
+def say_hi(request, name, age):
+    """Return a greeting"""
+    if age < 12:
+        message = "Sorry {}, you are not allowed here".format(name)
+    else:
+         message = "Hi {}, Welcome to Platzigram!.".format(name)
+    return HttpResponse(message)
