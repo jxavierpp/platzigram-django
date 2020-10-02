6605c63d6903b1b698a4dc7ecc588f64bb412f99
Xavier Paredes <jxavierpp1@gmail.com>
2020-10-01 23:57:20 -0700

    ♻ basic api example added

--- platzigram/views.py
+++ platzigram/views.py
@@ -5,8 +5,21 @@ from django.http import HttpResponse
 
 # Utilities
 from datetime import datetime
+import json
 
 
 def hello_word(request):
     """Return a greeting."""
-    return HttpResponse('Hello World!')
+    now = datetime.now().strftime("%b %dth, %Y - %H:%M hrs")
+    return HttpResponse("Oh, hi! Current server time is {now}".format(now=now))
+
+def hi(request):
+    """Hi"""
+    numbers = [int(number) for number in request.GET['numbers'].split(",")]
+    sorted_numbers = sorted(numbers)
+    data = {
+        'status': 'ok',
+        'numbers': sorted_numbers,
+        'message': 'Integers sorted successfuly.'
+    }
+    return HttpResponse(json.dumps(data), content_type='application/json')
