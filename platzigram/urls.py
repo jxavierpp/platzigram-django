815132b8b6c244dac4f108a3b0deb933555c100d
Xavier Paredes <jxavierpp1@gmail.com>
2020-10-02 00:04:35 -0700

    ♻ fixing method name issue

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -21,5 +21,5 @@ from platzigram import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("hello-world/", views.hello_word),
-    path("sorted/", views.sorted),
+    path("sorted/", views.sorted_numbers),
 ]
